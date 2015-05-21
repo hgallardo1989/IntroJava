2316e5187178a6e341bf8a1b6f7b980d5050e50e
Gabriela S. Soria <soria.gaby@gmail.com>
2015-05-21 17:29:40 -0400

    Prueba de tipos de clases

---
+++ src/py/com/uaa/pooj/util/tiposclases/Main.java
@@ -0,0 +1,9 @@
+package py.com.uaa.pooj.util.tiposclases;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Figura figu = new Figura();
+		Figura figu = new Cuadrado();
+	}
+}
