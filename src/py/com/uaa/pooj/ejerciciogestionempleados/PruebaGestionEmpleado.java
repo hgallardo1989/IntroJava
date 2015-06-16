e6a3ea8fd199c64c8dcbbc0679624ff5f58f85b3
Gabriela Soria <gsoria@localhost.localdomain>
2015-06-16 00:49:37 -0400

    ejercicios de gestion de hospital

---
+++ src/py/com/uaa/pooj/ejerciciogestionempleados/PruebaGestionEmpleado.java
@@ -0,0 +1,15 @@
+
+package py.com.uaa.pooj.ejerciciogestionempleados;
+
+/**
+ *
+ * @author gsoria
+ */
+public class PruebaGestionEmpleado {
+
+
+    public static void main(String[] args) {
+        // TODO Prueben los metodos implementados!
+    }
+    
+}
